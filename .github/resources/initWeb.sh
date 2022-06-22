da6b0b318fff7602657b5ab651526263485a6c5e
星冉 <troy@zygd.site>
2022-06-22 16:45:50 +0800

    add google

--- .github/resources/initWeb.sh
+++ .github/resources/initWeb.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 npm install github-md-html \
     && mkdir dist \
-    && cp google4999b474e9cdfd74.html dist/ \
+    && cp .github/resources/google4999b474e9cdfd74.html dist/ \
     && cp .github/resources/favicon.svg dist \
     && npx github-md-html --input=./README.md --output=./dist/index.html --title="Acunetix | Web Application Security Scanner" --keywords="Web 安全测试,渗透测试,Web 应用,awvs,awvs14" --description="Acunetix Web Vulnerability Scanner（AWVS）是用于测试和管理Web应用程序安全性的平台，能够自动扫描互联网或者本地局域网中是否存在漏洞，并报告漏洞" --icon="/favicon.svg"
