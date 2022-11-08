2cffe1290af77654d9f7916422fedbf23c3d288a
星冉 <Jalapeno1868@outlook.com>
2022-11-08 19:21:27 +0800

    update awvs 15

--- .github/resources/initWeb.sh
+++ .github/resources/initWeb.sh
@@ -2,4 +2,4 @@
 npm install github-md-html \
     && mkdir dist \
     && cp .github/resources/favicon.svg dist \
-    && npx github-md-html --input=./README.md --output=./dist/index.html --title="Acunetix | Web Application Security Scanner" --keywords="Web 安全测试,渗透测试,Web 应用,awvs,awvs14" --description="Acunetix Web Vulnerability Scanner（AWVS）是用于测试和管理Web应用程序安全性的平台，能够自动扫描互联网或者本地局域网中是否存在漏洞，并报告漏洞" --icon="/favicon.svg"
+    && npx github-md-html --input=./README.md --output=./dist/index.html --title="Acunetix | Web Application Security Scanner" --keywords="Web 安全测试,渗透测试,Web 应用,awvs,AWVS" --description="Acunetix Web Vulnerability Scanner（AWVS）是用于测试和管理Web应用程序安全性的平台，能够自动扫描互联网或者本地局域网中是否存在漏洞，并报告漏洞" --icon="/favicon.svg"
